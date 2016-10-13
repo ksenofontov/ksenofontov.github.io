b40b4f8d9aa85f7d59795d59893d09bf00603918
ksenofontov <dennisius@yahoo.com>
2016-10-13 20:26:54 +0300

    Add files via upload

---
+++ contact-form.php
@@ -0,0 +1,36 @@
+<?php
+ 
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$tel = htmlspecialchars($_POST["tel"]);
+$message = htmlspecialchars($_POST["message"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+ 
+/* Ваш адрес и тема сообщения */
+$address = "dennisius.com";
+$sub = "message from Chehiya";
+ 
+/* Формат письма */
+$mes = "Сообщение с сайта ХХХ.\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Телефон отправителя: $tel
+Сайт отправителя: $website
+Текст сообщения:
+$message";
+ 
+ 
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from  = "From: $name <$email> \r\n Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo 'Письмо отправлено, через 5 секунд вы вернетесь на сайт XXX';}
+else {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo 'Письмо не отправлено, через 5 секунд вы вернетесь на страницу YYY';}
+}
+exit; /* Выход без сообщения, если поле bezspama заполнено спам ботами */
+?>
